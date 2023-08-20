9cff6c1259ccc57dbb7c93cbca599ebe7b07d156
Muhammad Muzammil <126906348+muhammad-muzammil-123@users.noreply.github.com>
2023-08-20 09:46:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+function deleteButton(){
+const post = document.querySelector(".post");
+post.childNodes[1].innerHTML = "";
+
+}
+function editButton(){
+  
+const edit = document.querySelector(".post");
+ const para =  edit.childNodes[1]
+  const textArea = document.createElement("textarea");
+  textArea.appendChild(para);
+
+  
+}
+ 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
